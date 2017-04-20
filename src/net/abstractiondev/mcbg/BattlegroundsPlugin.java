630db884965c67818e3414c37ac41c3f260f84dc
Benjamin Lack <benjaminshlack@gmail.com>
2017-04-20 22:48:11 +0100

    Added onEnable and onDisable

--- src/net/abstractiondev/mcbg/BattlegroundsPlugin.java
+++ src/net/abstractiondev/mcbg/BattlegroundsPlugin.java
@@ -2,19 +2,19 @@ package net.abstractiondev.mcbg;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-@Override
-public void onEnable() {
-    //TODO
-        }
 
-public void onDisable() {
-    //TODO
-        }
 /*
  * Battlegrounds Plugin  
  */
 
 public class BattlegroundsPlugin
 {
+    @Override
+    public void onEnable() {
+        //TODO
+    }
 
+    public void onDisable() {
+        //TODO
+    }
 }
