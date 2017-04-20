271826f6d5e4c4c2260f8a422aa912dfc69613f3
Benjamin Lack <benjaminshlack@gmail.com>
2017-04-20 23:37:14 +0100

    Extended Java

--- src/net/abstractiondev/mcbg/BattlegroundsPlugin.java
+++ src/net/abstractiondev/mcbg/BattlegroundsPlugin.java
@@ -7,7 +7,7 @@ import org.bukkit.plugin.java.JavaPlugin;
  * Battlegrounds Plugin  
  */
 
-public class BattlegroundsPlugin
+public class BattlegroundsPlugin extends JavaPlugin
 {
     @Override
     public void onEnable() {
